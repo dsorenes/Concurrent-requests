3d1bd236703837a730bc2ffa8e1a226160fb0caf
Daniel Sørenes <danielsorenes@hotmail.no>
2020-01-25 05:48:49 +0100

    early version commited

---
+++ concurrent_requests.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import sys
+import requests
+import time
+from multiprocessing.dummy import Pool
+from urllib3.exceptions import InsecureRequestWarning
+
+session = requests.Session()
+session.verify = False
+
+
+# Suppress only the single warning from urllib3 needed.
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+def print_response(response):
+    url = response.url
+    status = response.status_code
+    elapsed_time = response.elapsed.total_seconds()
+    print(url, '\t', status, '\t\t', elapsed_time)
+
+def print_responses(responses):
+    print('method'.ljust(len(sys.argv[1])), '\t\t', 'status', '\t', 'response_time')
+    for response in responses:
+       print_response(response) 
+
+def make_requests(number_of_threads):
+    with Pool(number_of_threads) as pool:
+        start_time = time.perf_counter()
+        
+        responses = pool.imap_unordered(session.get, urls)
+        print_responses(responses)
+
+        end_time = time.perf_counter() - start_time
+
+    print('Total time elapsed: ', end_time)
+
+
+if __name__ == '__main__':
+    number_of_requests_to_make_per_thread = int(sys.argv[2])
+    number_of_threads = int(sys.argv[3])
+    
+    urls = [sys.argv[1]] * number_of_requests_to_make_per_thread * number_of_threads
+    print(len(urls))
+
+    make_requests(number_of_threads)
+
